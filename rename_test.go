b30835f5680ea506d44523ef437d1e782699a731
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2014-05-05 21:38:53 -0400

    initial tests for Rename
    Implemented a couple of tests for rename.  Also I discovered that if
    you project to a completely disparate type it doesn’t error, just
    returns a zero value.

---
+++ rename_test.go
@@ -0,0 +1,53 @@
+package rel
+
+import (
+	"testing"
+)
+
+// tests for projection
+func TestRename(t *testing.T) {
+
+	// TODO(jonlawlor): replace with table driven test?
+	type r1tup struct {
+		PNO int
+		SNO int
+		Qty int
+	}
+
+	r1 := orders.Rename(r1tup{})
+	if r1.GoString() != orders.GoString() {
+		t.Errorf("orders.Rename(PNO, SNO, Qty) = \"%s\", want \"%s\"", r1.GoString(), orders.GoString())
+
+	}
+	type r2tup struct {
+		PartNo   int
+		SupplyNo int
+		Quantity int
+	}
+
+	r2 := orders.Rename(r2tup{})
+	r2GoString := `rel.New([]struct {
+ PartNo   int 
+ SupplyNo int 
+ Quantity int 
+}{
+ {1, 1, 300, },
+ {1, 2, 200, },
+ {1, 3, 400, },
+ {1, 4, 200, },
+ {1, 5, 100, },
+ {1, 6, 100, },
+ {2, 1, 300, },
+ {2, 2, 400, },
+ {3, 2, 200, },
+ {4, 2, 200, },
+ {4, 4, 300, },
+ {4, 5, 400, },
+})`
+	if r2.GoString() != r2GoString {
+		t.Errorf("orders.Rename(PartNo, SupplyNo, Quantity) = \"%s\", want \"%s\"", r2.GoString(), r2GoString)
+
+	}
+
+	return
+}
