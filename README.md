d12161381eb9b4c4ae813cb7005fc3edb9c2f55d
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2014-06-08 23:02:32 -0400

    initial map implementation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 rel
 ========
 
-Relational Algebra in Go.
+Relational Algebra in Go.  Most Go doesn't need relational algebra, but most relational algebra needs Go.  The inherent parallelism in relational operations is buried by RDBMS's, and is much less extensible in function.  The cleanness of expression is missing in most ORMs, which prevents most query optimization.  Go provides the tools which can take advantage of both that parallelism and query optimization.  It is my hope that this package will prove that is true.
 
-This implements most (?) of the elements of relational algebra, including project, restrict, join, intersect, setdiff, and union.  It also implements some of the common non-relational operations, including groupby, order, insert, and update.  To learn more about relational algebra, C. J. Date's Database in Depth is a great place to start, and is used as the source of terminology in the rel package.
+This implements most of the traditional elements of relational algebra, including project, restrict, join, intersect, setdiff, and union.  It also implements some of the common non-relational operations, including groupby, map, and order.  To learn more about relational algebra, C. J. Date's Database in Depth is a great place to start, and is used as the source of terminology in the rel package.
 
-The semantics of this package are very similar to Microsoft's LINQ, although the syntax is different.
+The semantics of this package are very similar to Microsoft's LINQ, although the syntax is somewhat different.  This isn't LINQ though - it is a library, and it is not integrated with the language, which means rel has a significant performance cost relative to normal go code that doesn't use reflection.  On the plus side, you can see how it does everything.
 
 Interfaces
 ==========
@@ -29,12 +29,12 @@ TODOs
 + Add errors to indicate when relations are constructed from invalid operations
 + Implement tests for Err()
 + Implement tests for channel canceling
++ Implement tests for Map
 + Use the go race detector & clear up any issues.  (this requires a 64bit arch)
 + Reach 100% test coverage (currently 60%)
 + Implement tests with deterministic output of relational operations.  Currently tests for things like GoString, join, and groupby are dependent on the (arbitrary) order of output tuples.  They should go through an orderby operation first, or just compare against a known good relation through setdiff.
 + Implement sub packages for other data sources, such as csv readers, generic sql tables, json, or gob.
 + Implement non relational operations like order.
-+ Implement Map
 + Write better docs
 + Write single godoc file
 + Hook up chan_mem to some kind of copying mechanism.
---
+++ map.go
@@ -0,0 +1,200 @@
+// map implements a set mapping expression in relational algebra
+
+package rel
+
+// projection is a type that represents a project operation
+type MapExpr struct {
+	// the input relation
+	source1 Relation
+
+	// zero is the resulting relation tuple type
+	zero T
+
+	// the function that maps from source tuple type to result tuple type
+	fcn func(T) T
+
+	// set of candidate keys
+	cKeys CandKeys
+
+	// sourceDistinct indicates if the function results in distinct tuples or
+	// if a distinct has to be performed afterwards
+	isDistinct bool
+
+	err error
+}
+
+// Tuples sends each tuple in the relation to a channel
+// note: this consumes the values of the relation, and when it is finished it
+// closes the input channel.
+func (r *MapExpr) Tuples(t chan<- T) chan<- struct{} {
+	cancel := make(chan struct{})
+
+	if r.Err() != nil {
+		close(t)
+		return cancel
+	}
+
+	body1 := make(chan T)
+	bcancel := r.source1.Tuples(body1)
+
+	if r.isDistinct {
+		// assign fields from the old relation to fields in the new
+		// TODO(jonlawlor): add parallelism here
+		go func(body <-chan T, res chan<- T) {
+		Loop:
+			for {
+				select {
+				case tup1, ok := <-body:
+					if !ok {
+						break Loop
+					}
+					select {
+					// set the field in the new tuple to the value from the old one
+					case t <- r.fcn(tup1):
+					case <-cancel:
+						close(bcancel)
+						return
+					}
+				case <-cancel:
+					close(bcancel)
+					return
+				}
+			}
+			close(t)
+		}(body1, t)
+
+		return cancel
+	}
+	go func(body <-chan T, res chan<- T) {
+		m := map[interface{}]struct{}{}
+	Loop:
+		for {
+			select {
+			case tup1, ok := <-body:
+				if !ok {
+					break Loop
+				}
+				tup2 := r.fcn(tup1)
+				// set the field in the new tuple to the value from the old one
+				if _, isdup := m[tup2]; !isdup {
+					m[tup2] = struct{}{}
+					select {
+					case t <- tup2:
+					case <-cancel:
+						close(bcancel)
+						return
+					}
+				}
+			case <-cancel:
+				close(bcancel)
+				return
+			}
+		}
+		close(t)
+	}(body1, t)
+
+	return cancel
+
+}
+
+// Zero returns the zero value of the relation (a blank tuple)
+func (r *MapExpr) Zero() T {
+	return r.zero
+}
+
+// CKeys is the set of candidate keys in the relation
+func (r *MapExpr) CKeys() CandKeys {
+	return r.cKeys
+}
+
+// GoString returns a text representation of the Relation
+func (r *MapExpr) GoString() string {
+	return goStringTabTable(r)
+}
+
+// String returns a text representation of the Relation
+func (r *MapExpr) String() string {
+	return r.source1.String() + ".Map({" + HeadingString(r.source1) + "}->{" + HeadingString(r) + "})"
+}
+
+// Project creates a new relation with less than or equal degree
+// t2 has to be a new type which is a subdomain of r.
+func (r1 *MapExpr) Project(z2 T) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	return &ProjectExpr{r1, z2, nil}
+}
+
+// Restrict creates a new relation with less than or equal cardinality
+// p has to be a func(tup T) bool where tup is a subdomain of the input r.
+// This is a general purpose restrict - we might want to have specific ones for
+// the typical theta comparisons or <= <, =, >, >=, because it will allow much
+// better optimization on the source data side.
+func (r1 *MapExpr) Restrict(p Predicate) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	return &RestrictExpr{r1, p, nil}
+}
+
+// Rename creates a new relation with new column names
+// z2 has to be a struct with the same number of fields as the input relation
+// note: we might want to change this into a projectrename operation?  It will
+// be tricky to represent this in go's type system, I think.
+func (r1 *MapExpr) Rename(z2 T) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	return &RenameExpr{r1, z2, nil}
+}
+
+// Union creates a new relation by unioning the bodies of both inputs
+//
+func (r1 *MapExpr) Union(r2 Relation) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	if r2.Err() != nil {
+		return r2
+	}
+	return &UnionExpr{r1, r2, nil}
+}
+
+// SetDiff creates a new relation by set minusing the two inputs
+//
+func (r1 *MapExpr) SetDiff(r2 Relation) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	if r2.Err() != nil {
+		return r2
+	}
+	return &SetDiffExpr{r1, r2, nil}
+}
+
+// Join creates a new relation by performing a natural join on the inputs
+//
+func (r1 *MapExpr) Join(r2 Relation, zero T) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	if r2.Err() != nil {
+		return r2
+	}
+	return &JoinExpr{r1, r2, zero, nil}
+}
+
+// GroupBy creates a new relation by grouping and applying a user defined func
+//
+func (r1 *MapExpr) GroupBy(t2, vt T, gfcn func(<-chan T) T) Relation {
+	if r1.Err() != nil {
+		return r1
+	}
+	return &GroupByExpr{r1, t2, vt, gfcn, nil}
+}
+
+// Error returns an error encountered during construction or computation
+func (r1 *MapExpr) Err() error {
+	return r1.err
+}
